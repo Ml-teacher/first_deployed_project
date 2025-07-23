6a18c69d2d29b894ff3e12c70aa3a3b38b6fab19
Ml-teacher <samattemp840@gmail.com>
2025-07-23 18:45:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import category_encoders as ce
+import plotly.express as px
 
-st.title('🎈 App Name')
 
-st.write('Привет мир!')
+st.set_page_config(page_title="🐧 Penguin Classifier", layout="wide")
+st.title('🐧 Penguin Classifier - Обучение и предсказание')
+st.write("## Работа с датасетом пингвинов")
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
